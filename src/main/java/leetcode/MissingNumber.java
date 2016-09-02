0a104f06483a4db355f9f89d8f304f75c7e23a34
BillLin <bill33@LindeMacBook-Pro.local>
2016-09-02 22:20:03 +0800

    Complete a binary search version of Missing Number Fail because the input is disorder

--- src/main/java/leetcode/MissingNumber.java
+++ src/main/java/leetcode/MissingNumber.java
@@ -7,12 +7,28 @@ package leetcode;
  */
 public class MissingNumber {
     public int missingNumber(int[] nums) {
-        int sum1 = (1 + nums.length) * nums.length / 2;
-        int sum2 = 0;
-        for(int item : nums) {
-            sum2+=item;
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0] == 0 ? 1 : 0;
+
+        int ans = 0;
+        int left = 0;
+        int right = nums.length;
+        int mid =  (right - left) / 2;
+
+        while(left != right) {
+            if(nums[mid] != mid) { // drop right
+                ans = mid;
+                right = mid;
+                mid = (right - left) / 2;
+
+            } else { // drop left
+                left = mid + 1;
+                mid = left + (right - left) / 2;
+                ans = right;
+            }
         }
-        return sum1 - sum2;
+
+        return ans;
     }
 }
 
